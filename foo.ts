89a43133f91f5ee3670e04f39f3eeb7650e0b097
Matt McCutchen <rmccutch@mit.edu>
2016-02-25 03:25:52 -0500

    TypeScript test.

---
+++ foo.ts
@@ -0,0 +1,3 @@
+export function getMotd() {
+  return "hi";
+}
---
+++ typescript-test.ts
@@ -0,0 +1,28 @@
+import {getMotd} from './foo.ts';
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    motd: function() {
+      return getMotd();
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
